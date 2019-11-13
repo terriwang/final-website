f11ebaca3373cebcfa98b89f33344ddd15c2eea7
terrinobb@gmail.com <qwerewq12321>
2019-11-13 16:50:12 +1100

    commit

--- final signup.js
+++ final signup.js
@@ -1,9 +1,29 @@
 function myFunction() {
-  var name=document.next["next"]["fname"];
+  var fn =document.forms["myForm"]["fname"].value;
+  var ln =document.forms["myForm"]["lName"].value;
+  var email =document.forms["myForm"]["email"].value;
+  var phone =document.forms["myForm"]["phone"].value;
+  var bth =document.forms["myForm"]["dateOfBirth"].value;
 
   
-  if (name == "") {
-    alert("Name must be filled out");
+  if (fn == "") {
+    alert("Please fill out your first name.");
     return false;
   }   
+  if (ln == "") {
+    alert("Please fill out your last name.");
+    return false;
+  }  
+  if (email == "") {
+    alert("Please fill out your email.");
+    return false;
+  }  
+  if (phone == "") {
+    alert("Please fill out your phone number.");
+    return false;
+  }  
+  if (bth == "") {
+    alert("Please fill out your birthday.");
+    return false;
+  }  
 }
